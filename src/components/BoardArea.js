671b67c699af7921ab25caa5fd7bd324ec792968
Patrick Sullivan <patrick@jsullivan.org>
2022-02-08 18:09:20 -0800

    Fixed useEffect to listen for keydown events and change the gameboard state for the current squares, plus track a pointer for current location of gameboard squares matrix

--- src/components/BoardArea.js
+++ src/components/BoardArea.js
@@ -2,93 +2,101 @@ import React, { useState, useEffect } from 'react';
 import LetterBox from './LetterBox.js';
 
 const BoardArea = (props) => {
-	const [squares, setSquares] = useState([[], [], [], [], [], [], []]);
-	const [currentSquare, setCurrentSquare] = useState([0, 0]);
+  const [gameBoard, setGameBoard] = useState({
+    squares: [[], [], [], [], [], [], []],
+    currentSquare: [0, 0],
+  });
 
-	useEffect(() => {
-		window.addEventListener('keydown', (e) => {
-			if (e.key.match(/^[A-Za-z]{1}$/g)) {
-				console.log(e.key);
-				setSquares(squares[currentSquare[0]][currentSquare[1]], e.key);
-				if (currentSquare[1] % 6 === 5) {
-					setCurrentSquare([currentSquare[0]++, 0]); //
-				} else {
-					setCurrentSquare([currentSquare[0], currentSquare[1]++]);
-				}
-				// } else if (e.key === 'Backspace') {
-				// 					dispatch(removeLetter())
-				// 			} else if (e.key === 'Enter') {
-				// 					dispatch(makeGuess())
-			}
-			console.log(currentSquare);
-		});
-	}, []);
+  useEffect(() => {
+    window.addEventListener('keydown', (e) => {
+      if (e.key.match(/^[A-Za-z]{1}$/g)) {
+        console.log('Keydown Event: ', e.key);
+        setGameBoard((prevState) => {
+          console.log('Current squares: ', prevState.squares);
+          prevState.squares[prevState.currentSquare[0]][prevState.currentSquare[1]] = e.key;
 
-	const renderSquare = (char) => {
-		return <LetterBox letter={char} onKeyPress={() => props.onClick()} />;
-	};
+          if (prevState.currentSquare[1] % 6 === 5) {
+            console.log('Changing current row', prevState.currentSquare[0] + 1);
+            prevState.currentSquare = [prevState.currentSquare[0] + 1, 0];
+          } else {
+            console.log('Changing current column', prevState.currentSquare[1] + 1);
+            prevState.currentSquare = [prevState.currentSquare[0], prevState.currentSquare[1] + 1];
+          }
+          return prevState;
+          // } else if (e.key === 'Backspace') {
+          // 					dispatch(removeLetter())
+          // 			} else if (e.key === 'Enter') {
+          // 					dispatch(makeGuess())
+        });
+      }
+    });
+  }, [gameBoard]);
 
-	return (
-		<div className="board-area">
-			<div>{props.finalWord}</div>
-			<div className="word-row">
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-			</div>
-			<div className="word-row">
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-			</div>
-			<div className="word-row">
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-			</div>
-			<div className="word-row">
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-			</div>
-			<div className="word-row">
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-			</div>
-			<div className="word-row">
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-			</div>
-			<div className="word-row">
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-				{renderSquare()}
-			</div>
-		</div>
-	);
+  const renderSquare = (char) => {
+    return <LetterBox letter={char} onKeyPress={() => props.onClick()} />;
+  };
+
+  return (
+    <div className="board-area">
+      <div>{props.finalWord}</div>
+      <div className="word-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="word-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="word-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="word-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="word-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="word-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="word-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+    </div>
+  );
 };
 
 export default BoardArea;
