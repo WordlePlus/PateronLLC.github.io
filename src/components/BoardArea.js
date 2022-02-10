b7f9e4c4d2dd798f77a9abc898b2c2c030a29168
Cameron Simmons <cssim22@gmail.com>
2022-02-09 21:20:41 -0700

    working as before this commit with consolidated code using map functions rather than explicitly typing out elements in BoardArea

--- src/components/BoardArea.js
+++ src/components/BoardArea.js
@@ -4,21 +4,21 @@ import LetterBox from './LetterBox.js';
 const BoardArea = (props) => {
 	const [gameBoard, setGameBoard] = useState({
 		squares: [
-      Array(6).fill(''), 
-      Array(6).fill(''),
-      Array(6).fill(''),
-      Array(6).fill(''),
-      Array(6).fill(''),
-      Array(6).fill(''),
-      Array(6).fill('')],
+			Array(6).fill(''),
+			Array(6).fill(''),
+			Array(6).fill(''),
+			Array(6).fill(''),
+			Array(6).fill(''),
+			Array(6).fill(''),
+			Array(6).fill(''),
+		],
 		currentSquare: [0, 0],
 	});
 
 	useEffect(() => {
-		document.addEventListener('keydown', (e) => {
+		const handleKeyDown = (e) => {
 			if (e.key.match(/^[A-Za-z]{1}$/g)) {
 				console.log('Keydown Event: ', e.key);
-				console.log(gameBoard.squares[gameBoard.currentSquare[0][0]]);
 				setGameBoard((prevState) => {
 					console.log('Current squares: ', prevState.squares);
 					prevState.squares[prevState.currentSquare[0]][
@@ -26,13 +26,8 @@ const BoardArea = (props) => {
 					] = e.key;
 
 					if (prevState.currentSquare[1] % 6 === 5) {
-						console.log('Changing current row', prevState.currentSquare[0] + 1);
 						prevState.currentSquare = [prevState.currentSquare[0] + 1, 0];
 					} else {
-						console.log(
-							'Changing current column',
-							prevState.currentSquare[1] + 1
-						);
 						prevState.currentSquare = [
 							prevState.currentSquare[0],
 							prevState.currentSquare[1] + 1,
@@ -49,51 +44,27 @@ const BoardArea = (props) => {
 					// 					dispatch(makeGuess())
 				});
 			}
-		});
-	}, [gameBoard]);
+		};
+		document.addEventListener('keydown', handleKeyDown);
+	}, []);
 
-	const renderSquare = (char) => {
-		return <LetterBox key={Math.floor(Math.random()*10000)} letter={char} />;
+	const renderSquare = (char, key) => {
+		return <LetterBox key={Math.floor(Math.random() * 100000)} letter={char} />;
 	};
 
 	return (
 		<div className="board-area">
 			<div>{props.finalWord}</div>
-			<div className="word-row">
-				{gameBoard.squares[0].map((char) => {
-					return renderSquare(char);
-				})}
-			</div>
-			<div className="word-row">
-        {gameBoard.squares[1].map((char) => {
-					return renderSquare(char);
-				})}
-			</div>
-			<div className="word-row">
-        {gameBoard.squares[2].map((char) => {
-					return renderSquare(char);
-				})}
-			</div>
-			<div className="word-row">
-        {gameBoard.squares[0].map((char) => {
-					return renderSquare(char);
-				})}
-			</div>
-			<div className="word-row">
-        {gameBoard.squares[0].map((char) => {
-					return renderSquare(char);
-				})}
-			</div>
-			<div className="word-row">
-        {gameBoard.squares[0].map((char) => {
-					return renderSquare(char);
-				})}
-			</div>
-			<div className="word-row">
-        {gameBoard.squares[0].map((char) => {
-					return renderSquare(char);
-				})}
-			</div>
+			{gameBoard.squares.map((el, idx) => {
+				const row = idx;
+				return (
+					<div className="word-row" key={idx}>
+						{gameBoard.squares[row].map((char, idx) => {
+							return renderSquare(char, idx * 100);
+						})}
+					</div>
+				);
+			})}
 		</div>
 	);
 };
