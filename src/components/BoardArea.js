a8bcabd254ca57006ae4431940332ad51953a9aa
Cameron Simmons <cssim22@gmail.com>
2022-02-16 10:22:51 -0700

    created reset board function to be used on game win or lose.

--- src/components/BoardArea.js
+++ src/components/BoardArea.js
@@ -5,29 +5,42 @@ const BoardArea = ({ setFinalWord, finalWord }) => {
   const [squares, setSquares] = useState(
     Array.from({ length: 2 }, (value, index) => Array.from({ length: 6 }, (value1, index1) => '')),
   );
-  const [squareColor, setSquareColor] = useState(
+  const [squareColors, setSquareColors] = useState(
     Array.from({ length: 2 }, (value, index) =>
       Array.from({ length: 6 }, (value1, index1) => 'gray'),
     ),
   );
   const [currSquare, setCurrSquare] = useState([0, 0]);
 
-  console.log('finalWord in boardarea: ', finalWord);
-
+  
   useEffect(() => {
+    const resetBoard = () =>{
+      const newSquares = Array.from({ length: 2 }, (value, index) =>
+        Array.from({ length: 6 }, (value1, index1) => ''),
+        );
+      const newSquareColors = Array.from({ length: 2 }, (value, index) =>
+        Array.from({ length: 6 }, (value1, index1) => 'gray'),
+        );
+      const newCurrSquare = [0, 0];
+      setSquares(()=>newSquares);
+      setSquareColors(()=>newSquareColors);
+      setCurrSquare(()=>newCurrSquare);
+      setFinalWord();
+    }
+    
     document.addEventListener('keydown', handleKeyDown);
 
     function handleKeyDown({ key }) {
       let newSquares = squares.slice();
       let newCurrSquare = currSquare.slice();
-      console.log({ key });
 
       switch (true) {
         case /^[A-Za-z]{1}$/g.test(key):
           if (newCurrSquare[1] < squares[0].length) {
             newSquares[newCurrSquare[0]][newCurrSquare[1]] = key;
-
             newCurrSquare[1]++;
+            setSquares(() => newSquares);
+            setCurrSquare(() => newCurrSquare);
           }
           break;
 
@@ -35,55 +48,55 @@ const BoardArea = ({ setFinalWord, finalWord }) => {
           if (newCurrSquare[1] !== 0) {
             newSquares[newCurrSquare[0]][newCurrSquare[1] - 1] = '';
             newCurrSquare[1]--;
+            setSquares(() => newSquares);
+            setCurrSquare(() => newCurrSquare);
           } else {
             console.log('hi, Cameron is right');
           }
           break;
-
+        
         case (key === 'Enter' || key === 'Return') && currSquare[1] === squares[0].length:
-          // color changing logic here?
+          //color the keys in each row 
           newSquares[newCurrSquare[0]].forEach((square, squareIndex) => {
             if (square === finalWord[squareIndex]) {
-              setSquareColor((state) => {
+              setSquareColors((state) => {
                 let newColors = state.slice();
-                console.log(newColors);
                 newColors[newCurrSquare[0]][squareIndex] = 'green';
+                return newColors;
+              });
+            } else if (finalWord.includes(square)){
+              setSquareColors((state) => {
+                let newColors = state.slice();
+                newColors[newCurrSquare[0]][squareIndex] = 'yellow';
                 return newColors;
               });
             }
           });
 
+          //check to see if the row matches the final word
           const rowWord = newSquares[newCurrSquare[0]].join('');
           if (rowWord === finalWord) {
             console.log(rowWord, 'You are a wizard.  You are a winner.  Here is 1 buttcoin⭐');
-            setFinalWord();
-            newSquares = Array.from({ length: 2 }, (value, index) =>
-              Array.from({ length: 6 }, (value1, index1) => ''),
-            );
-            newCurrSquare = [0, 0];
+            resetBoard()
           } else if (newCurrSquare[0] >= squares.length - 1) {
             console.log('This is what you are: loser LOSER loser');
-            setFinalWord();
-            newSquares = Array.from({ length: 2 }, (value, index) =>
-              Array.from({ length: 6 }, (value1, index1) => ''),
-            );
-            newCurrSquare = [0, 0];
+            resetBoard();
           } else {
             newCurrSquare[0]++;
             newCurrSquare[1] = 0;
+            setSquares(() => newSquares);
+            setCurrSquare(() => newCurrSquare);
           }
           break;
+
         default:
           console.log("Key down event didn't match");
-      }
-      console.log('squarecolor', squareColor);
-      setSquares(() => newSquares);
-      setCurrSquare(() => newCurrSquare);
+        }
     }
 
     // clear event listener
     return () => document.removeEventListener('keydown', handleKeyDown);
-  }, [finalWord, setFinalWord, currSquare, squares]);
+  }, [finalWord, setFinalWord, currSquare, squares, squareColors]);
 
   const renderSquare = (char, key, color) => {
     return <LetterBox key={key} char={char} color={color} />;
@@ -97,7 +110,7 @@ const BoardArea = ({ setFinalWord, finalWord }) => {
           <div className="word-row" key={rowIndex}>
             {rowArray.map((char, colIndex) => {
               const key = rowIndex.toString() + colIndex.toString();
-              return renderSquare(char, key, squareColor[rowIndex][colIndex]);
+              return renderSquare(char, key, squareColors[rowIndex][colIndex]);
             })}
           </div>
         );
